3889b35dbf618d56772f716a79132eaf84e2f0b3
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2014-01-30 15:01:46 -0500

    cleaned up table view

--- schedule.rb
+++ schedule.rb
@@ -7,5 +7,7 @@ get '/' do
   @start = Date.new(year,1,1).end_of_week
   @end = Date.new(year+1,1,1).end_of_week
   @weeks = ((@end - @start) / 7.0).to_i
+  people = %W[mitch bill chris robert tony suzanne simpson]
+  slots = %W[monday_morning monday_afternoon tuesday_morning tuesday_afternoon wednesday_morning wednesday_afternoon thursday_morning thursday_afternoon friday_morning friday_afternoon]
   erb :'index.html', :layout => :'layout.html'
 end

a6f41e58d885ed32fc0139bf296ebea18f3a87a2
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2014-01-30 14:09:53 -0500

    added a layout and index erb pages

--- schedule.rb
+++ schedule.rb
@@ -1,6 +1,10 @@
 #!/usr/bin/env ruby
 require 'sinatra'
+require 'active_support/all'
 
 get '/' do
-  erb :index
+  year = 2014
+  @start = Date.new(year,1,1).end_of_week
+  @end = Date.new(year+1,1,1).end_of_week
+  erb :'index.html', :layout => :'layout.html'
 end

f061b3ba58b530c8c97c6f61ca30d37f194cca61
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2014-01-30 17:25:26 -0500

    shift distribution evens out over 56 weeks

--- schedule.rb
+++ schedule.rb
@@ -10,7 +10,7 @@ get '/' do
   @weeks = ((@end - @start) / 7.0).to_i
   @people = %W[mitch bill chris robert tony suzanne simpson]
   @slots = %W[monday_morning monday_afternoon tuesday_morning tuesday_afternoon wednesday_morning wednesday_afternoon thursday_morning thursday_afternoon friday_morning friday_afternoon]
-  @preferred_slots = @people.zip([5, 3, 8, 6, 1, 4, 2])
+  @preferred_slots = @people.zip([5, 3, 7, 6, 1, 4, 2])
   @shift = Shift.new people: @people, slots: @slots, preferred_slots: @preferred_slots
 
   erb :'index.html', :layout => :'layout.html'

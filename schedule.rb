e81c145974dfae111d173f5246cd2dea2e781159
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2014-01-30 14:39:26 -0500

    simple table printing

--- schedule.rb
+++ schedule.rb
@@ -6,5 +6,6 @@ get '/' do
   year = 2014
   @start = Date.new(year,1,1).end_of_week
   @end = Date.new(year+1,1,1).end_of_week
+  @weeks = ((@end - @start) / 7.0).to_i
   erb :'index.html', :layout => :'layout.html'
 end

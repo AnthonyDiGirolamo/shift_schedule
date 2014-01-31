1f47f92ec672607e57910cf29cb78c1d973e0ac9
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2014-01-31 11:47:42 -0500

    precalculate schedules and dry things up

--- schedule.rb
+++ schedule.rb
@@ -3,20 +3,49 @@ require 'sinatra'
 require 'active_support/all'
 require_relative 'lib/shift'
 
-post '/' do
-  logger.info params
 
-  @year = params["year"].to_i
+def calculate_shifts
+  @shift = Shift.new people: @people, slots: @slots, preferred_slots: @preferred_slots
+
+  @shift_counts = {}
+  @shift_counts["empty"] = 0
+  @people.each do |person|
+    @shift_counts[person] = 0
+  end
+
+  @shifts = []
+
+  for week in 0..(@weeks-1)
+    @shifts[week] = {dates: [], schedule: @shift.schedule(week)}
+
+    for day in [1,2,3,4,5]
+      @shifts[week][:dates] << (@start + (week*7).days + day.days).strftime("%A - %m/%d/%Y")
+    end
+
+    @shifts[week][:schedule].each do |person|
+      @shift_counts[person] += 1
+    end
+  end
+end
+
+before do
+  @slots = %W[monday_morning monday_afternoon tuesday_morning tuesday_afternoon wednesday_morning wednesday_afternoon thursday_morning thursday_afternoon friday_morning friday_afternoon]
+end
+
+post '/' do
+  # logger.info params
+
+  @year = params.fetch("year", Time.new.year).to_i
   @start = Date.new(@year,1,1).end_of_week
-  @weeks = params["weeks"].to_i
+  @weeks = params.fetch("weeks", 56).to_i
   @end = @start + (@weeks * 7).days
   @slots = %W[monday_morning monday_afternoon tuesday_morning tuesday_afternoon wednesday_morning wednesday_afternoon thursday_morning thursday_afternoon friday_morning friday_afternoon]
 
   @people = params["person"]["name"].values.reject{|i| i.blank?}
-
   @preferred_slots = @people.zip( params["person"]["shift"].values[0,@people.size].collect{|i|i.to_i} )
 
-  @shift = Shift.new people: @people, slots: @slots, preferred_slots: @preferred_slots
+  calculate_shifts
+
   erb :'index.html', :layout => :'layout.html'
 end
 
@@ -26,9 +55,9 @@ get '/' do
   @end = Date.new(@year+1,1,1).end_of_week
   @weeks = ((@end - @start) / 7.0).to_i
   @people = %W[mitch bill chris robert tony suzanne simpson]
-  @slots = %W[monday_morning monday_afternoon tuesday_morning tuesday_afternoon wednesday_morning wednesday_afternoon thursday_morning thursday_afternoon friday_morning friday_afternoon]
   @preferred_slots = @people.zip([5, 3, 7, 6, 1, 4, 2])
-  @shift = Shift.new people: @people, slots: @slots, preferred_slots: @preferred_slots
+
+  calculate_shifts
 
   erb :'index.html', :layout => :'layout.html'
 end

d1e6a7c7e661635f39916624a30efe5a753f47bb
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2014-01-31 09:54:18 -0500

    hooked up form to params

--- schedule.rb
+++ schedule.rb
@@ -3,6 +3,24 @@ require 'sinatra'
 require 'active_support/all'
 require_relative 'lib/shift'
 
+post '/' do
+  logger.info params
+
+  year = 2014
+  @start = Date.new(year,1,1).end_of_week
+  @end = Date.new(year+1,1,1).end_of_week
+  @weeks = ((@end - @start) / 7.0).to_i
+
+  @slots = %W[monday_morning monday_afternoon tuesday_morning tuesday_afternoon wednesday_morning wednesday_afternoon thursday_morning thursday_afternoon friday_morning friday_afternoon]
+
+  @people = params["person"]["name"].values.reject{|i| i.blank?}
+
+  @preferred_slots = @people.zip( params["person"]["shift"].values[0,@people.size].collect{|i|i.to_i} )
+
+  @shift = Shift.new people: @people, slots: @slots, preferred_slots: @preferred_slots
+  erb :'index.html', :layout => :'layout.html'
+end
+
 get '/' do
   year = 2014
   @start = Date.new(year,1,1).end_of_week
@@ -15,3 +33,4 @@ get '/' do
 
   erb :'index.html', :layout => :'layout.html'
 end
+

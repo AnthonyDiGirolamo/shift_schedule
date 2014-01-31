d35893761172cc8fad6660579c9a2610ef9e8182
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2014-01-31 12:24:38 -0500

    titleize some strings

--- schedule.rb
+++ schedule.rb
@@ -29,7 +29,7 @@ def calculate_shifts
 end
 
 before do
-  @slots = %W[monday_morning monday_afternoon tuesday_morning tuesday_afternoon wednesday_morning wednesday_afternoon thursday_morning thursday_afternoon friday_morning friday_afternoon]
+  @slots = %W[monday_morning monday_afternoon tuesday_morning tuesday_afternoon wednesday_morning wednesday_afternoon thursday_morning thursday_afternoon friday_morning friday_afternoon].collect{|s| s.titleize}
 end
 
 post '/' do
@@ -39,7 +39,6 @@ post '/' do
   @start = Date.new(@year,1,1).end_of_week
   @weeks = params.fetch("weeks", 56).to_i
   @end = @start + (@weeks * 7).days
-  @slots = %W[monday_morning monday_afternoon tuesday_morning tuesday_afternoon wednesday_morning wednesday_afternoon thursday_morning thursday_afternoon friday_morning friday_afternoon]
 
   @people = params["person"]["name"].values.reject{|i| i.blank?}
   @preferred_slots = @people.zip( params["person"]["shift"].values[0,@people.size].collect{|i|i.to_i} )
@@ -54,7 +53,7 @@ get '/' do
   @start = Date.new(@year,1,1).end_of_week
   @end = Date.new(@year+1,1,1).end_of_week
   @weeks = ((@end - @start) / 7.0).to_i
-  @people = %W[mitch bill chris robert tony suzanne simpson]
+  @people = %W[mitch bill chris robert tony suzanne simpson].collect{|n| n.titleize}
   @preferred_slots = @people.zip([5, 3, 7, 6, 1, 4, 2])
 
   calculate_shifts

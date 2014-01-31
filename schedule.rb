33a7168d7f727f0f157a8fd4b7b65bfea85cb187
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2014-01-31 14:06:28 -0500

    fixed a bug, shifts class is not immutable for some reason

--- schedule.rb
+++ schedule.rb
@@ -3,10 +3,11 @@ require 'sinatra'
 require 'active_support/all'
 require_relative 'lib/shift'
 
+before do
+  @slots = %W[monday_morning monday_afternoon tuesday_morning tuesday_afternoon wednesday_morning wednesday_afternoon thursday_morning thursday_afternoon friday_morning friday_afternoon].collect{|s| s.titleize}
+end
 
 def calculate_shifts
-  @shift = Shift.new people: @people, slots: @slots, preferred_slots: @preferred_slots
-
   @shift_counts = {}
   @shift_counts["empty"] = 0
   @people.each do |person|
@@ -16,6 +17,7 @@ def calculate_shifts
   @shifts = []
 
   for week in 0..(@weeks-1)
+    @shift = Shift.new people: @people, slots: @slots, preferred_slots: @preferred_slots
     @shifts[week] = {dates: [], schedule: @shift.schedule(week)}
 
     for day in [1,2,3,4,5]
@@ -28,23 +30,29 @@ def calculate_shifts
   end
 end
 
-before do
-  @slots = %W[monday_morning monday_afternoon tuesday_morning tuesday_afternoon wednesday_morning wednesday_afternoon thursday_morning thursday_afternoon friday_morning friday_afternoon].collect{|s| s.titleize}
-end
-
 post '/' do
-  # logger.info params
+  logger.info params
 
   @year = params.fetch("year", Time.new.year).to_i
   @start = Date.new(@year,1,1).end_of_week
   @weeks = params.fetch("weeks", 56).to_i
   @end = @start + (@weeks * 7).days
 
-  @people = params["slot"]["name"].values.reject{|i| i.blank?}
-  logger.info @people
-  @preferred_slots = params["slot"]["name"].collect{|slot_id, name| [name, slot_id.to_i] unless name.blank? }.compact
-  logger.info @preferred_slots
-  @people_slots = params["slot"]["name"].values
+  @people = []
+  for i in 0..9
+    @people << params["slot_#{i}"]
+  end
+  @people_slots = @people.dup
+  @people.reject!{|n| n.blank?}
+
+  @preferred_slots = []
+  @people_slots.each_with_index do |name, index|
+     @preferred_slots << [name, index] unless name.blank?
+  end
+
+  # logger.info @people_slots
+  # logger.info @people
+  # logger.info @preferred_slots
 
   calculate_shifts
 
@@ -56,11 +64,19 @@ get '/' do
   @start = Date.new(@year,1,1).end_of_week
   @end = Date.new(@year+1,1,1).end_of_week
   @weeks = ((@end - @start) / 7.0).to_i
-  @people = %W[mitch bill chris simpson tony suzanne robert].collect{|n| n.titleize}
-  logger.info @people
-  @preferred_slots = @people.zip([5, 3, 7, 6, 1, 4, 2])
-  logger.info @preferred_slots
-  @people_slots = ["", "mitch", "bill", "chris", "simpson", "tony", "suzanne", "robert", "", ""]
+
+  @people_slots = ["", "tony", "robert", "bill", "suzanne", "mitch", "simpson", "chris", "", ""].collect{|n| n.titleize}
+
+  @people = @people_slots.dup
+  @people.reject!{|n| n.blank?}
+  @preferred_slots = []
+  @people_slots.each_with_index do |name, index|
+     @preferred_slots << [name, index] unless name.blank?
+  end
+
+  # logger.info @people_slots
+  # logger.info @people
+  # logger.info @preferred_slots
 
   calculate_shifts
 
